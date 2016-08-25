39d12707ed9436f8d1dbb9ab35bfe85ad35fa73f
Brent Stineman <brent.@stinemans.org>
2016-08-25 16:07:26 -0400

    updated presentation and added demo files

---
+++ Presentations/ContainerDemoApp/ContainerDemoApp/src/ContainerDemoApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContainerDemoApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("**********************************");
+            for (int x = 0; x<=5; x++)
+                Console.WriteLine("**");
+
+            // insert message here
+            Console.WriteLine("*****    Hello there!");
+
+            for (int x = 0; x <= 5; x++)
+                Console.WriteLine("**");
+            Console.WriteLine("**********************************");
+        }
+    }
+}
