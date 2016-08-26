186ea4df12c9b06740f626d17e878e11fe8e6e56
Brent Stineman <brent.@stinemans.org>
2016-08-26 06:30:23 -0400

    various updates and the addition of the powershell demo script

---
+++ Presentations/Container Demo Script.ps1
@@ -0,0 +1,47 @@
+﻿# setup
+# pull the following images
+#
+# run/start the following images
+#
+
+##
+### Slide 17
+##
+
+# list images in local repository
+docker images
+
+# get a docker image from docker hub
+docker pull microsoft/sample-dotnet:latest
+
+# remove a local image
+docker rmi 
+
+##
+### Slide 18
+##
+
+# create and run an image
+docker run microsoft/sample-dotnet:latest
+
+# list containers
+docker ps 
+
+# list all containers (including those not running)
+docker ps -a
+
+# run an existing, stopped container (do from cmd prompt)
+docker start -ia 
+
+# stop a running container 
+docker stop 
+
+##
+### Slide 19
+##
+
+# remove an existing container
+docker rm <container>
+
+# remove all containers
+docker ps –a -–format “{{.ID}}” | %{docker rm $_ -f}
