9524dcb37d4cec5e02165fdd6cbd23a1575d2944
Brent Stineman <brent@stinemans.org>
2018-01-31 12:48:52 -0600

    various update

--- Presentations/Service Fabric/RealWorldDemo/APIBackEnd/Controllers/ValuesController.cs
+++ Presentations/Service Fabric/RealWorldDemo/APIBackEnd/Controllers/ValuesController.cs
@@ -13,7 +13,7 @@ namespace APIBackEnd.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "Magnus", "Ville" };
         }
 
         // GET api/values/5
